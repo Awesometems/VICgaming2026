80db54a6b4f060edd3a77e14b9d89554de45eee3
Carlos Fernandez <cfernandez06@vt.edu>
2026-03-29 09:42:30 -0400

    Implemented notebook

--- scripts/tracking.gd
+++ scripts/tracking.gd
@@ -3,3 +3,4 @@ extends Node2D
 var crowbar = 0
 var key = 0
 var gear = 0
+var notes = ["", "", "", "", ""]
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -5,6 +5,18 @@ extends Control
 
 func _ready() -> void:
 	room_name_label.text = Map.room_names[Player.currentLocation]
+	itemcheck()
+	popnotes()
+	$Notebook.visible = false
+
+func popnotes():
+	$"Notebook/Control/1".text = Tracking.notes[0]
+	$"Notebook/Control/2".text = Tracking.notes[1]
+	$"Notebook/Control/3".text = Tracking.notes[2]
+	$"Notebook/Control/4".text = Tracking.notes[3]
+	$"Notebook/Control/5".text = Tracking.notes[4]
+
+func itemcheck():
 	if !Tracking.crowbar:
 		$crowbar.disabled = true
 		$crowbar.visible = false
@@ -14,7 +26,7 @@ func _ready() -> void:
 	if !Tracking.gear:
 		$gear.disabled = true
 		$gear.visible = false
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -30,3 +42,7 @@ func _on_right_pressed() -> void:
 func _on_left_pressed() -> void:
 	Player.move(Player.Direction.LEFT)
 	
+
+
+func _on_notes_pressed() -> void:
+	$Notebook.visible = !$Notebook.visible
